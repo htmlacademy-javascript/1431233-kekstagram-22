050afb15d8a6eccfc9b4245d1a06ba2169836c35
HermanMR <hermanvfx@outlook.com>
2021-01-31 21:01:14 +0300

    Функции

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Функция, возвращающая случайное целое число из переданного диапазона включительно.
+const getRandomNumber = function(min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+};
+// Функция для проверки максимальной длины строки.
+const maxLengthString = function (checkedString, maxLength) {
+  if (checkedString.length <= maxLength) {
+    return true;
+  } else {
+    alert('ОШИБКА - Превышено максимальное количество символов');
+  }
+};
