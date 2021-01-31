d252dc7702dd06f8f21bfab723f4b38b08bf433a
Keks <keks@htmlacademy.ru>
2021-01-31 22:30:13 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -3,14 +3,10 @@ const getRandomNumber = function(min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
 };
 // Функция для проверки максимальной длины строки.
-const maxLengthString = function (checkedString, maxLength) {
-  if (checkedString.length <= maxLength) {
-    return true;
-  } else {
-    return false;
-  }
+const checkMaxLengthString = function (checkedString, maxLength) {
+  return checkedString.length <= maxLength;
 };
 
 // Присвоил значение что бы не ругалось
 getRandomNumber(1 , 3);
-maxLengthString('test', 5);
+checkMaxLengthString('test', 5);
