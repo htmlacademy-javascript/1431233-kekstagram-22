8a60f485bb71dd5a9c344809d980249100156ee7
Keks <keks@htmlacademy.ru>
2021-02-17 22:14:59 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/uploadingPhotos.js
+++ 5/js/uploadingPhotos.js
@@ -3,7 +3,7 @@ const uploadPhotos = document.querySelector('#upload-file');
 const photosUploadOverlay = document.querySelector('.img-upload__overlay');
 const uploadCancel = document.querySelector('#upload-cancel');
 // Функция закрытия окна нажатием Escape
-const modalEscape = function (evt) {
+const escPress = function (evt) {
   if (evt.key === 'Escape') {
     evt.preventDefault();
     closeModal();
@@ -13,13 +13,13 @@ const modalEscape = function (evt) {
 const openModal = function () {
   photosUploadOverlay.classList.remove('hidden');
   document.querySelector('body').classList.add('modal-open');
-  document.addEventListener('keydown', modalEscape);
+  document.addEventListener('keydown', escPress);
 };
 // Закрытие окна
 const closeModal = function () {
   photosUploadOverlay.classList.add('hidden');
   document.querySelector('body').classList.remove('modal-open');
-  document.removeEventListener('keydown', modalEscape);
+  document.removeEventListener('keydown', escPress);
   uploadPhotos.value = '';
 };
 // Экспорт
